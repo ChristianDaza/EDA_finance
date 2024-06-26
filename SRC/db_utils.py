0c18ca510d7d8e6901e75a33be42cc9a257debcd
Christian <ch.arenasdaza@gmail.com>
2024-06-26 18:23:59 +0100

    Create read_credentials() for yaml file

--- SRC/db_utils.py
+++ SRC/db_utils.py
@@ -1,3 +1,26 @@
 #%%
 class RDSDatabaseConnector:
     pass
+# %%
+# Read yaml credetial files    
+import yaml
+def read_credentials(db_credentials_file_yaml):
+    """
+    This function:
+        Reads a yaml file with credentials to connect to a database and returns them as a dictionary.
+    Prameters:
+        db_credentials_file (str): 
+            Path to the yaml file with the credentilas to connact to a data base.
+    Returns: 
+        redentials_dict (dict):
+            Python dictionary that contains the credetails to connact to a data base.
+    """
+    
+    with open(db_credentials_file_yaml) as file:
+        credentials_dict = yaml.safe_load(file)
+    return credentials_dict
+
+
+# %%
+read_credentials("/Users/ChAre/OneDrive/Desktop/aicore/EDA_finance/credentials.yaml")
+# %%
