688cb73599ee02c900f9059fd940cd08a9ee04cb
Christian <ch.arenasdaza@gmail.com>
2024-06-26 19:49:12 +0100

    Update connect_sqlalchemy()

--- SRC/db_utils.py
+++ SRC/db_utils.py
@@ -1,11 +1,10 @@
 #%%
-import psycopg2
-from sqlalchemy import create_engine
 class RDSDatabaseConnector:
     def __init__(self, credentials):
         self.credentials = credentials
 
-    def init_sqlalchemy(self, credentials):
+    def connect_sqlalchemy(credentials):
+            from sqlalchemy import create_engine
             """
             This function:
             initialises sql alchemy and connect it wiht the database in aws.
@@ -17,7 +16,7 @@ class RDSDatabaseConnector:
             DATABASE_TYPE = 'postgresql'
             DBAPI = 'psycopg2'
             ENDPOINT = credentials['RDS_HOST']
-            USER = 'postgres'
+            USER = credentials['RDS_USER']
             PASSWORD = credentials['RDS_PASSWORD']
             PORT = credentials['RDS_PORT']
             DATABASE = credentials['RDS_DATABASE']
@@ -45,31 +44,7 @@ def read_credentials(db_credentials_file_yaml):
 
 
 # %%
-read_credentials("/Users/ChAre/OneDrive/Desktop/aicore/EDA_finance/credentials.yaml")
+credentials = read_credentials("")        
 # %%
-def init_sqlalchemy(credentials):
-            from sqlalchemy import create_engine
-            """
-            This function:
-            initialises sql alchemy and connect it wiht the database in aws.
-            
-            Prameters:
-                credentials (dict):
-                    Python dictionary that contains the credetails to connact to a data base.
-            """
-            DATABASE_TYPE = 'postgresql'
-            DBAPI = 'psycopg2'
-            ENDPOINT = credentials['RDS_HOST']
-            USER = 'postgres'
-            PASSWORD = credentials['RDS_PASSWORD']
-            PORT = credentials['RDS_PORT']
-            DATABASE = credentials['RDS_DATABASE']
-            engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-            engine.connect()
-# %%
-credentials = read_credentials("/Users/ChAre/OneDrive/Desktop/aicore/EDA_finance/credentials.yaml")
-# init_sqlalchemy(credentials)
-# %%
-credentials
-#engine.close()
+connect_sqlalchemy(credentials)
 # %%
