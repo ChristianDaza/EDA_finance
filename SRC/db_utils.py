9ac7766644ca418993f9f48e1ec0b2d72f670e21
Christian <ch.arenasdaza@gmail.com>
2024-06-26 19:19:20 +0100

    Testing functions

--- SRC/db_utils.py
+++ SRC/db_utils.py
@@ -47,3 +47,27 @@ def read_credentials(db_credentials_file_yaml):
 # %%
 read_credentials("/Users/ChAre/OneDrive/Desktop/aicore/EDA_finance/credentials.yaml")
 # %%
+def init_sqlalchemy(credentials):
+            from sqlalchemy import create_engine
+            """
+            This function:
+            initialises sql alchemy and connect it wiht the database in aws.
+            
+            Prameters:
+                credentials (dict):
+                    Python dictionary that contains the credetails to connact to a data base.
+            """
+            DATABASE_TYPE = 'postgresql'
+            ENDPOINT = credentials['RDS_HOST']
+            USER = 'postgres'
+            PASSWORD = credentials['RDS_PASSWORD']
+            PORT = credentials['RDS_PORT']
+            DATABASE = credentials['RDS_DATABASE']
+            engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+            engine.connect()
+# %%
+credentials = read_credentials("/Users/ChAre/OneDrive/Desktop/aicore/EDA_finance/credentials.yaml")
+init_sqlalchemy(credentials)
+
+#engine.close()
+# %%
