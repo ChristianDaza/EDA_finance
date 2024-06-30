8f8510165a56a0a9456a1ff282b0c51bebd2bc24
Christian <ch.arenasdaza@gmail.com>
2024-06-30 13:53:54 +0100

    Update space in the docstrings.

--- SRC/pre_processing.py
+++ SRC/pre_processing.py
@@ -31,15 +31,17 @@ class DataTransform:
     def __init__(self, dataframe):
         self.dataframe = dataframe
 
-    def category_convert(self, columns):
+    def category_transform(self, columns):
         """
         This function:
             Transforms specified dataframe column or columns into categorical data.
+        
         Prameters:
             dataframe(df): 
                 Dataframe with the desired colum or columns to transform into categorical data.
             columns (list): 
                 List of name or names as strings of columns to change into catgorical data.
+        
         Returns: 
             dataframe (df):
                 Dataframe wiht specified column or columns changed into categorical data.
@@ -48,15 +50,17 @@ class DataTransform:
             self.dataframe[column] = self.dataframe[column].astype("category")
         return self.dataframe
     
-    def date_convert(self, columns):
+    def date_transform(self, columns):
         """ 
         This function:
             Transforms specified columns that contain dates in string form into date type data.
+        
         Prameters:
             dataframe(df): 
                 Dataframe with the desired colum or columns to transform into date type data.
             columns (list): 
                 List of name or names of columns to change into date type data.
+        
         Returns: 
             dataframe (df):
                 Dataframe with specified column or columns transformed into date type data.
@@ -69,6 +73,7 @@ class DataTransform:
         """ 
         This function:
             Formats data type dates in dataframe columns.
+
         Prameters:
             dataframe(df): 
                 Dataframe with the desired date types colum or columns to format.
@@ -77,6 +82,7 @@ class DataTransform:
             Format (str):
                 String specifying the format the user what the date type columns to changed into following datatime module symbols.
                 %Y (year), %m (month), %d(day): one or all symbols ordered wihthin the same string with or without a delimiter between them.
+        
         Returns: 
             dataframe (df):
                 Dataframe with specified date type column or columns reformatted.
@@ -85,15 +91,17 @@ class DataTransform:
             self.dataframe[column]= self.dataframe[column].dt.strftime(format)
         return self.dataframe
     
-    def string_tranform(self, columns):
+    def string_transform(self, columns):
         """ 
         This function:
             Transforms the specified column or columns into the string data type.
+        
         Prameters:
             dataframe(df): 
                 Dataframe with the desired date types colum or columns to format.
             columns (list): 
                 List of column name or names to format.
+        
         Returns: 
             dataframe (df):
                 Dataframe with the values in the specified column or columns transform into strings.
@@ -102,15 +110,17 @@ class DataTransform:
             self.dataframe[column] = self.dataframe[column].astype("string")
         return self.dataframe
     
-    def numeric_tranform(self, columns, numeric_type):
+    def numeric_transform(self, columns, numeric_type):
         """ 
         This function:
             Transforms the specified column or columns into the numeric data type.
+        
         Prameters:
             dataframe(df): 
                 Dataframe with the desired column to chnage the data type.
             columns (list): 
                 List of column name or names to format.
+        
         Returns: 
             dataframe (df):
                 Dataframe with the values in the specified column or columns transform into numeric data type.
@@ -123,6 +133,7 @@ class DataTransform:
         """ 
         This function:
             Remove a charcter or characters from the specified dataframe column.
+        
         Prameters:
             dataframe (df): 
                 Dataframe with the desired colum or columns to transform into date type data.
@@ -130,6 +141,7 @@ class DataTransform:
                 Name of column from which the characters are going to be removed.
             Character (str):
                 Charcters to removed from the specified dataframe column.
+        
         Returns: 
             dataframe (df):
                 Dataframe with the specified characters removed.
@@ -142,6 +154,7 @@ class DataTransform:
         """ 
         This function:
             Changes the name of a specified column.
+       
         Prameters:
             dataframe(df): 
                 Dataframe with the desired colum to change the name.
@@ -151,6 +164,7 @@ class DataTransform:
                 Name of the column the user's want to change.
         new_column_name (str):
                 The name the user wants the column to be.
+        
         Returns:
             dataframe (df):
                 Dataframe with the columns new name.
