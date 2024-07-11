26829611612afe0fd59b83575de85e82c9c95272
Christian <ch.arenasdaza@gmail.com>
2024-07-11 22:50:31 +0100

    Update plot.py docstring.

--- SRC/plot.py
+++ SRC/plot.py
@@ -1,28 +1,33 @@
-import seaborn as sns
+# Imports
 import matplotlib.pyplot as plt
-from statsmodels.graphics.gofplots import qqplot
 from matplotlib import pyplot
-import matplotlib.pyplot as plt
 import seaborn as sns
+from statsmodels.graphics.gofplots import qqplot
 
-
-class  Plotter:
+# Class
+class Plotter:
 
     """
-    Allows the use to visualise the data
+    Allows the use to visualise the data.
 
     Parameter:
         dataframe (df):
             Dataframe from which information will be use to create the plots.
+
+    Attributes:
+        dataframe (df): 
+            Dataframe to extract information from.
     
     Methods:
         missing_values_plot:
-            Creates a density plot of missing values for all columns of a selected dataframe
+            Creates a barplot of missing values for all columns of a selected dataframe.
+
         hist_plot:
             Creates a histogram from provided data
-        plot_qq: Creates a quantile-quantile plot from provided data.
 
-    
+        plot_qq: 
+            Creates a quantile-quantile plot from provided data.
+
     """
     
     def __init__(self, dataframe):
@@ -32,7 +37,7 @@ class  Plotter:
     def missing_values_plot(self):
         """
         This function:
-            Creates a barplot based on the based on the proprotion of missing data per column, which the plot fucntion calculates.
+            Creates a barplot based the proprotion of missing data per column, which the plot function calculates.
         """
         plt.figure(figsize=(12,8))
         sns.displot(
@@ -43,6 +48,7 @@ class  Plotter:
             aspect=1.05
         )
 
+
     def hist_plot(self, dataframe, column, bins_size=10, title ="", xlabel="", ylabel="", kde_option = False):
         """
         This function:
@@ -51,19 +57,26 @@ class  Plotter:
         Prameters:
             Dataframe (df):
                 Where the desired data is store.
+
             Column (str):
                 Name of the dataframe column to create the histogram from.
+
             bin_size (str):
                 Number of bins the histagram will show.
+
             title (str):
                 Title of the histogram.
+
             xlabel (str):
                 Name of the x-axis.
+
             ylabel (str):
                 Name of the y-axis.
+
             kde_option (bool):
                 cComputes a kernel density estimate to smooth the distribution and show on the plot as a line.
         """
+
         plt.figure(figsize=(10, 6))
         sns.histplot(data=dataframe, x=column, bins= bins_size, kde=kde_option)
         plt.title(title)
@@ -80,11 +93,14 @@ class  Plotter:
         Prameters:
             Dataframe_column (df):
                 Dataframe wich contains the data to plot.
+
             column (df):
                 Column of the dataframe use to generate the plot.
+
             Scale_option (int):
                 Scale parameter for distribution.
         """
+
         qqplot(dataframe[column], scale=scale_option ,line='q')
         pyplot.show()
 
@@ -92,17 +108,21 @@ class  Plotter:
     def box_plot(self, dataframe, column, title ="", xlabel="", color= "deepskyblue"):
         """
         This function:
-            Creates a histogram from provided data.
+            Creates a boxplot from provided data.
 
         Prameters:
             Dataframe (df):
                 Where the desired data is store.
+
             Column (str):
                 Name of the dataframe column to create the boxplot from.
+
             title (str):
                 Title of the boxplot.
+
             xlabel (str):
-                Name of the x-axis
+                Name of the x-axis.
+
             color (string):
                 Name of the color that will be used for the inside of the boxplot and the outline of the outliers.
         """
